01b700bef4c819f13c5670386b93e9417787818c
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2024-09-04 17:09:50 +0100

    New translations en_us.ts (English) [ci skip]

--- tagstudio/resources/translations/en_US.ts
+++ tagstudio/resources/translations/en_US.ts
@@ -6,42 +6,42 @@
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="14"/>
       <source>MainWindow</source>
-      <translation type="unfinished">MainWindow</translation>
+      <translation>MainWindow</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="43"/>
       <source>And (includes all tags)</source>
-      <translation type="unfinished">And (includes all tags)</translation>
+      <translation>And (includes all tags)</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="48"/>
       <source>Or (includes any tag)</source>
-      <translation type="unfinished">Or (includes any tag)</translation>
+      <translation>Or (includes any tag)</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="77"/>
       <source>Thumbnail Size</source>
-      <translation type="unfinished">Thumbnail Size</translation>
+      <translation>Thumbnail Size</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="156"/>
       <source>&lt;</source>
-      <translation type="unfinished">&lt;</translation>
+      <translation>&lt;</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="182"/>
       <source>&gt;</source>
-      <translation type="unfinished">&gt;</translation>
+      <translation>&gt;</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="201"/>
       <source>Search Entries</source>
-      <translation type="unfinished">Search Entries</translation>
+      <translation>Search Entries</translation>
     </message>
     <message>
       <location filename="tagstudio/src/qt/ui/home.ui" line="220"/>
       <source>Search</source>
-      <translation type="unfinished">Search</translation>
+      <translation>Search</translation>
     </message>
   </context>
 </TS>
