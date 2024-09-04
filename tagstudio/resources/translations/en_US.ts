7041a9bbd5f5dd4784a8c9283e50d42f0330bb16
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2024-09-04 15:58:37 +0100

    New translations en_us.ts (English) [ci skip]

---
+++ tagstudio/resources/translations/en_US.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="14"/>
+      <source>MainWindow</source>
+      <translation type="unfinished">MainWindow</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="43"/>
+      <source>And (includes all tags)</source>
+      <translation type="unfinished">And (includes all tags)</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="48"/>
+      <source>Or (includes any tag)</source>
+      <translation type="unfinished">Or (includes any tag)</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="77"/>
+      <source>Thumbnail Size</source>
+      <translation type="unfinished">Thumbnail Size</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="156"/>
+      <source>&lt;</source>
+      <translation type="unfinished">&lt;</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="182"/>
+      <source>&gt;</source>
+      <translation type="unfinished">&gt;</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="201"/>
+      <source>Search Entries</source>
+      <translation type="unfinished">Search Entries</translation>
+    </message>
+    <message>
+      <location filename="tagstudio/src/qt/ui/home.ui" line="220"/>
+      <source>Search</source>
+      <translation type="unfinished">Search</translation>
+    </message>
+  </context>
+</TS>
